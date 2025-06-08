a98ded5c31febcca6d25aab5931f1a75881e7ad8
Cookie-1412 <albert.jonathan0406@gmail.com>
2025-06-08 20:16:16 +0800

    Add animating class

--- Scenes/MenuScene.cpp
+++ Scenes/MenuScene.cpp
@@ -5,12 +5,15 @@
 #include "Engine/GameEngine.hpp"
 #include "UI/Label.hpp"
 #include "UI/TextButton.hpp"
+#include "UI/Video.hpp"
 
 void MenuScene::Initialize() {
     int w = Engine::GameEngine::GetInstance().GetScreenSize().x;
     int h = Engine::GameEngine::GetInstance().GetScreenSize().y;
     int halfW = w / 2;
     int halfH = h / 2;
+
+    AddNewObject(new Engine::Video("menu_background.png", 0, 0, 480, 270, 0, 0, w, h, 14));
 
     AddNewObject(new Engine::Label("<insert game name here>", "Minecraft.ttf",
         100, halfW, halfH - 50, 20, 255, 20, 255, 0.5, 1));
