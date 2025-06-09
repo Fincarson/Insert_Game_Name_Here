68b7662cc2b0a01be5e57675716d5921d5670c47
Cookie-1412 <albert.jonathan0406@gmail.com>
2025-06-10 04:17:27 +0800

    Added parent weapon file

---
+++ Weapons/Weapon.hpp
@@ -0,0 +1,41 @@
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+
+#include <string>
+#include <allegro5/allegro.h>
+#include "Engine/Point.hpp"
+#include "Engine/Angle.hpp"
+#include "Engine/MouseState.hpp"
+
+/// Base class for all weapons. Handles aiming, cooldown, and firing logic.
+class Weapon {
+public:
+    /// ctor: load weapon and bullet bitmaps, set cooldown (s), bullet speed (px/s), damage
+    Weapon(const std::string& weaponImagePath,
+           const std::string& bulletImagePath,
+           float cooldown,
+           float bulletSpeed,
+           int damage);
+    virtual ~Weapon();
+
+    /// Update weapon position & angle; fire on left mouse down when cooldown allows
+    void Update(const Engine::Point& newPosition);
+    /// Draw weapon rotated to current angle
+    void Draw() const;
+
+protected:
+    /// Spawn a bullet at `pos` heading at `angle`; to be implemented by each weapon type
+    virtual void SpawnBullet(const Engine::Point& pos, float angle) = 0;
+
+    ALLEGRO_BITMAP* weaponBitmap;
+    ALLEGRO_BITMAP* bulletBitmap;
+    float cooldownTime;
+    float bulletSpeed;
+    int damage;
+    double lastShotTime;
+
+    Engine::Point position;
+    float angle;
+};
+
+#endif //WEAPON_HPP
